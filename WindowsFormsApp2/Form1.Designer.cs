14a1b7a658b579bfe19ccf3833221783ddb2112c
philippe <philippe.corrihons@pole-emploi.fr>
2019-09-02 16:34:47 +0200

    TimerApp bugs correction

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApp2
 {
-    partial class Form1
+    partial class TimerApp
     {
         /// <summary>
         /// Required designer variable.
@@ -28,144 +28,198 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
+            this.titre_Heure = new System.Windows.Forms.Label();
+            this.heure = new System.Windows.Forms.Label();
+            this.nbreIteration = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
+            this.tempsEcoule = new System.Windows.Forms.Label();
+            this.cancelButton = new System.Windows.Forms.Button();
             this.label9 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // startButton
             // 
-            this.button1.Location = new System.Drawing.Point(80, 78);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(64, 38);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Démarrer";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 62);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(207, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Cliquer sur le bouton pour démarrer le timer";
+            this.startButton.Location = new System.Drawing.Point(106, 138);
+            this.startButton.Margin = new System.Windows.Forms.Padding(4);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(85, 47);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Démarrer";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(194, 18);
+            this.label2.Location = new System.Drawing.Point(259, 22);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(72, 26);
+            this.label2.Size = new System.Drawing.Size(88, 31);
             this.label2.TabIndex = 2;
             this.label2.Text = "Timer";
             // 
-            // label3
+            // titre_Heure
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(31, 136);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(37, 13);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Heure";
+            this.titre_Heure.AutoSize = true;
+            this.titre_Heure.Location = new System.Drawing.Point(41, 209);
+            this.titre_Heure.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.titre_Heure.Name = "titre_Heure";
+            this.titre_Heure.Size = new System.Drawing.Size(47, 17);
+            this.titre_Heure.TabIndex = 3;
+            this.titre_Heure.Text = "Heure";
             // 
-            // label4
+            // heure
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(54, 158);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "label4";
+            this.heure.AutoSize = true;
+            this.heure.Location = new System.Drawing.Point(63, 236);
+            this.heure.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.heure.Name = "heure";
+            this.heure.Size = new System.Drawing.Size(47, 17);
+            this.heure.TabIndex = 4;
+            this.heure.Text = "Heure";
             // 
-            // label5
+            // nbreIteration
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(333, 158);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 13);
-            this.label5.TabIndex = 5;
-            this.label5.Text = "label5";
+            this.nbreIteration.AutoSize = true;
+            this.nbreIteration.Location = new System.Drawing.Point(444, 236);
+            this.nbreIteration.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.nbreIteration.Name = "nbreIteration";
+            this.nbreIteration.Size = new System.Drawing.Size(113, 17);
+            this.nbreIteration.TabIndex = 5;
+            this.nbreIteration.Text = "Nombre Itération";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(333, 136);
+            this.label6.Location = new System.Drawing.Point(444, 209);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(85, 13);
+            this.label6.Size = new System.Drawing.Size(113, 17);
             this.label6.TabIndex = 6;
             this.label6.Text = "Nombre Itération\r\n";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(187, 136);
+            this.label7.Location = new System.Drawing.Point(248, 209);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(75, 13);
+            this.label7.Size = new System.Drawing.Size(98, 17);
             this.label7.TabIndex = 7;
             this.label7.Text = "Temps Ecoulé";
             // 
-            // label8
+            // tempsEcoule
             // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(187, 158);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(35, 13);
-            this.label8.TabIndex = 8;
-            this.label8.Text = "label8";
+            this.tempsEcoule.AutoSize = true;
+            this.tempsEcoule.Location = new System.Drawing.Point(248, 236);
+            this.tempsEcoule.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.tempsEcoule.Name = "tempsEcoule";
+            this.tempsEcoule.Size = new System.Drawing.Size(97, 17);
+            this.tempsEcoule.TabIndex = 8;
+            this.tempsEcoule.Text = "Temps ecoulé";
             // 
-            // button2
+            // cancelButton
             // 
-            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.button2.Location = new System.Drawing.Point(336, 77);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(69, 39);
-            this.button2.TabIndex = 9;
-            this.button2.Text = "Cancel";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Location = new System.Drawing.Point(447, 137);
+            this.cancelButton.Margin = new System.Windows.Forms.Padding(4);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(92, 48);
+            this.cancelButton.TabIndex = 9;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(333, 62);
+            this.label9.Location = new System.Drawing.Point(443, 118);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(37, 13);
+            this.label9.Size = new System.Drawing.Size(48, 17);
             this.label9.TabIndex = 10;
             this.label9.Text = "Pause";
             // 
-            // Form1
+            // label5
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(40, 288);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Status";
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(103, 288);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(45, 17);
+            this.labelStatus.TabIndex = 12;
+            this.labelStatus.Text = "Statut";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(106, 356);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(433, 23);
+            this.progressBar1.TabIndex = 13;
+            // 
+            // progressBar
+            // 
+            this.progressBar.AutoSize = true;
+            this.progressBar.Location = new System.Drawing.Point(234, 326);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(123, 17);
+            this.progressBar.TabIndex = 14;
+            this.progressBar.Text = "Barre Progression";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(103, 118);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Démarrer";
+            // 
+            // TimerApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.CancelButton = this.button2;
-            this.ClientSize = new System.Drawing.Size(441, 212);
+            this.CancelButton = this.cancelButton;
+            this.ClientSize = new System.Drawing.Size(620, 414);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label9);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.label8);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.tempsEcoule);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
+            this.Controls.Add(this.nbreIteration);
+            this.Controls.Add(this.heure);
+            this.Controls.Add(this.titre_Heure);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.startButton);
             this.HelpButton = true;
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "TimerApp";
+            this.Text = "Timer Application";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.TimerApp_FormClosed);
+            this.Load += new System.EventHandler(this.TimerApp_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -173,17 +227,21 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button startButton;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label titre_Heure;
+        private System.Windows.Forms.Label heure;
+        private System.Windows.Forms.Label nbreIteration;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label tempsEcoule;
+        private System.Windows.Forms.Button cancelButton;
         private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label progressBar;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -11,86 +11,183 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp2
 {
-    public partial class Form1 : Form
+    public partial class TimerApp : Form
     {
         public int counter;
 
-        public bool IsCanceled { get; private set; }
-
-        public Form1()
+        private BackgroundWorker worker;
+        private System.Windows.Forms.Timer timer1;
+        private DateTime heureDebut;
+        private TimeSpan heureIntervalle;
+        public TimerApp()
         {
             InitializeComponent();
+            heureDebut = DateTime.Now;
+            heure.Text = heureDebut.ToString("dd/MM/yy HH:mm");
+            nbreIteration.Text = "";
+            tempsEcoule.Text = "";
+            //Configurarion timer events 
+            worker = new BackgroundWorker();
+            worker.DoWork += new DoWorkEventHandler(worker_DoWork);
+            worker.ProgressChanged += new ProgressChangedEventHandler(worker_ProgressChanged);
+            worker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(worker_RunWorkerCompleted);
+            worker.WorkerReportsProgress = true;
+            worker.WorkerSupportsCancellation = true;
+            //Timer configuration
+            timer1 = new System.Windows.Forms.Timer();
+           // timer1.Enabled = true;
+           // timer1.Start();
+            timer1.Interval = 1000; //
+            timer1.Tick += new EventHandler(Timer1_Tick);
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            label5.Text = "En cours";
-            counter = counter +1;
-            label4.Text= DateTime.Now.ToString("dd/mm/yy HH:mm");
-            Loop();
-         //   Thread thread = new Thread(this.Loop);
-         //   thread.Start();
-        }
-
-        private void Loop()
-        {
-            double Indice = 0;
-            IsCanceled = false;
-           
-                label4.Text = DateTime.Now.ToString("dd/MM/yy HH:mm");
-                label4.Refresh();
-                label5.Text = Indice.ToString();
-                label5.Refresh();
-             //   toto2 = DateTime.Now - toto1; 
-             //   label8.Text = toto2.ToString(@"hh\:mm\:ss");
-                label8.Refresh();
-
-                Thread _thread = new Thread(loopThread);
-                _thread.Start();
-                Indice = Indice + 1;       
-        }
-
-         private void loopThread()
-         {
-            DateTime toto = new DateTime();
-            DateTime toto1 = new DateTime();
-            TimeSpan toto2 = new TimeSpan();
-            toto1 = DateTime.Now;
-            toto = DateTime.Now;
-            toto = toto.AddHours(3);
-            while (toto > DateTime.Now && IsCanceled == false)
-
-            {
-                Thread.Sleep(100000);
-                SendKeys.SendWait("{DOWN}");
-            }
-        }
-      
-        
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            // label5.Text = "En pause";
-            //  label4.Text = DateTime.Now.ToString("dd/mm/yy HH:mm");
-            IsCanceled = true;
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
+        private void TimerApp_Load(object sender, EventArgs e)
         {
 
         }
 
-        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        private void TimerApp_FormClosed(object sender, FormClosedEventArgs e)
         {
-            DialogResult dialog = MessageBox.Show("exit?", "exit", MessageBoxButtons.YesNo);
-            if (dialog ==DialogResult.Yes)
+            DialogResult dialog = MessageBox.Show("Exit?", "Exit", MessageBoxButtons.YesNo);
+            if (dialog == DialogResult.Yes)
             {
                 Application.Exit();
             }
             else if (dialog == DialogResult.No)
             {
-                IsCanceled = true;
             }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            //Change the status of the buttons on the UI accordingly
+            //The start button is disabled as soon as the background operation is started
+            //The Cancel button is enabled so that the user can stop the operation 
+            //at any point of time during the execution
+            startButton.Enabled = false;
+            cancelButton.Enabled = true;
+            timer1.Start();
+
+            // Kickoff the worker thread to begin it's DoWork function.
+            worker.RunWorkerAsync();
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            if (worker.IsBusy)
+            {
+                // Notify the worker thread that a cancel has been requested.
+                // The cancel will not actually happen until the thread in the
+                // DoWork checks the m_oWorker.CancellationPending flag. 
+                timer1.Stop();
+                worker.CancelAsync();
+            }
+        }
+            // <summary>
+            // On completed do the appropriate task
+            // </summary>
+            // <param name="sender"></param>
+            // <param name="e"></param>
+            void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            // The background process is complete. We need to inspect
+            // our response to see if an error occurred, a cancel was
+            // requested or if we completed successfully.  
+            if (e.Cancelled)
+            {
+                labelStatus.Text = "Task Cancelled.";
+            }
+
+            // Check to see if an error occurred in the background process.
+
+            else if (e.Error != null)
+            {
+                labelStatus.Text = "Error while performing background operation.";
+            }
+            else
+            {
+                // Everything completed normally.
+                labelStatus.Text = "Task Completed...";
+            }
+            //Change the status of the buttons on the UI accordingly
+            startButton.Enabled = true;
+            cancelButton.Enabled = false;
+        }
+
+        // <summary>
+        // Notification is performed here to the progress bar
+        // </summary>
+        // <param name="sender"></param>
+        // <param name="e"></param>
+        void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            // This function fires on the UI thread so it's safe to edit
+            // the UI control directly, no funny business with Control.Invoke :)
+            // Update the progressBar with the integer supplied to us from the
+            // ReportProgress() function.  
+
+            heureIntervalle = DateTime.Now - heureDebut;
+            double  toto = heureIntervalle.TotalHours;
+            tempsEcoule.Text = heureIntervalle.ToString();
+            nbreIteration.Text = e.ProgressPercentage.ToString();
+          //  progressBar1.Value = progressBar1.Value + 1;
+            labelStatus.Text = "Processing......" + "%";
+        }
+
+        // <summary>
+        // Time consuming operations go here </br>
+        // i.e. Database operations,Reporting
+        // </summary>
+        // <param name="sender"></param>
+        // <param name="e"></param>
+        void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int indice = 0;              
+         //   DateTime heureDebutIteration = new DateTime();
+         //   heureDebutIteration = DateTime.Now;
+         //   heureDebutIteration = heureDebutIteration.AddHours(3);
+            worker.ReportProgress(indice);
+            e.Cancel = false;
+
+            while (!e.Cancel)                //heureDebutIteration > DateTime.Now
+            {      
+                    Thread.Sleep(1000); //100000
+                    SendKeys.SendWait("{DOWN}");
+                    indice = indice + 1;
+                    worker.ReportProgress(indice);
+                    if (worker.CancellationPending)
+                    {
+                        // Set the e.Cancel flag so that the WorkerCompleted event
+                        // knows that the process was cancelled.
+                        e.Cancel = true;
+                        worker.ReportProgress(0);
+                        return;
+                    }
+            }           
+
+            // The sender is the BackgroundWorker object we need it to
+            // report progress and check for cancellation.
+            //NOTE : Never play with the UI thread here...
+
+            // Periodically report progress to the main thread so that it can
+            // update the UI.  In most cases you'll just need to send an
+            // integer that will update a ProgressBar                    
+                
+            // Periodically check if a cancellation request is pending.
+            // If the user clicks cancel the line
+            // m_AsyncWorker.CancelAsync(); if ran above.  This
+            // sets the CancellationPending to true.
+            // You must check this flag in here and react to it.
+            // We react to it by setting e.Cancel to true and leaving
+                
+            //Report 100% completion on operation completed
+            worker.ReportProgress(100);
+        }
+
+        void Timer1_Tick(object Sender, EventArgs e)
+        {
+            progressBar1.Value = progressBar1.Value + 1;
+          
         }
     }
 }
